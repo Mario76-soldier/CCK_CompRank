1cde816e120e2016aa32e0995082fbe37471852f
Mario76-soldier <2001cupid@gmail.com>
2023-12-29 23:00:21 +0900

    Initiate

---
+++ src/test/java/CubingClubKorea/CCK_CompRank/CckCompRankApplicationTests.java
@@ -0,0 +1,13 @@
+package CubingClubKorea.CCK_CompRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CckCompRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
